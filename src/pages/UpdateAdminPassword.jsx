018b707b56abb9a13d9ed92526f7edb1ad23a943
hyemin <ohyemin1113@gmail.com>
2025-09-20 20:34:16 +0900

    feat : 관리자 비밀번호 변경 페이지 구현

---
+++ src/pages/UpdateAdminPassword.jsx
@@ -0,0 +1,62 @@
+import LabeledInput from '../components/PasswordUpdateForm/LabeledInput';
+import Button from '../components/Button';
+import PasswordForm from '../components/PasswordUpdateForm/PasswordForm';
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import { IoIosArrowBack } from 'react-icons/io';
+
+const UpdatePassword = () => {
+  const [currentPW, setCurrentPW] = useState('');
+  const [newPW, setNewPW] = useState('');
+  const [confirmPW, setConfirmPW] = useState('');
+
+  return (
+    <>
+      <BackBtn>
+        <IoIosArrowBack />
+      </BackBtn>
+      <PasswordForm>
+        <Title>관리자 비밀번호 변경</Title>
+        <LabeledInput
+          label="현재 비밀번호"
+          placeholder="현재 비밀번호를 입력하세요."
+          value={currentPW}
+          onChange={(e) => setCurrentPW(e.target.value)}
+        />
+        <LabeledInput
+          label="새로운 비밀번호"
+          placeholder="변경할 비밀번호를 입력하세요."
+          value={newPW}
+          onChange={(e) => setNewPW(e.target.value)}
+        />
+        <LabeledInput
+          label="새로운 비밀번호 확인"
+          placeholder="변경할 비밀번호를 다시 입력하세요."
+          value={confirmPW}
+          onChange={(e) => setConfirmPW(e.target.value)}
+        />
+        <Button>변경</Button>
+      </PasswordForm>
+    </>
+  );
+};
+
+export default UpdatePassword;
+
+const Title = styled.h2`
+  text-align: center;
+  color: #2e2e32;
+  margin-bottom: 30px;
+  font-size: 32px;
+  font-weight: 700;
+`;
+
+const BackBtn = styled.button`
+  position: absolute;
+  left: 50px;
+  top: 50px;
+  font-size: 60px;
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
