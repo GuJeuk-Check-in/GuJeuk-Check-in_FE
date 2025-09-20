1eac9f1a12b87f304cad3cc676e2dc3d70f8334f
hyemin <ohyemin1113@gmail.com>
2025-09-20 21:10:11 +0900

    feat : PasswordForm 컴포넌트에 동적 width, height 속성 추가

--- src/components/PasswordForm.jsx
+++ src/components/PasswordForm.jsx
@@ -1,16 +1,24 @@
 import styled from '@emotion/styled';
 
-const PasswordForm = ({ children }) => {
-  return <FormContainer>{children}</FormContainer>;
+const PasswordForm = ({ children, width, height }) => {
+  return (
+    <FormContainer $width={width} $height={height}>
+      {children}
+    </FormContainer>
+  );
 };
 export default PasswordForm;
 
 const FormContainer = styled.div`
   box-sizing: border-box;
-  width: 499px;
-  height: 600px;
+  width: ${(props) => props.$width || '499px'};
+  height: ${(props) => props.$height || '600px'};
   background-color: #95d5f8;
   border-radius: 36px;
-  margin: 130px auto;
   padding: 70px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
 `;
