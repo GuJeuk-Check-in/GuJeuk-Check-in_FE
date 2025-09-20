1f21daa388f06d0bff95601bfa1a0c312f46e3a6
hyemin <ohyemin1113@gmail.com>
2025-09-20 21:10:47 +0900

    feat : 관리자 비밀번호 입력 페이지 구현

--- src/pages/EnterAdminPassword.jsx
+++ src/pages/EnterAdminPassword.jsx
@@ -1,17 +1,22 @@
 import LabeledInput from '../components/LabeledInput';
 import PasswordForm from '../components/PasswordForm';
+import Button from '../components/Button';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
 const EnterAdminPassword = () => {
+  const [currentPW, setCurrentPW] = useState('');
   return (
-    <PasswordForm>
+    <PasswordForm width="499px" height="400px">
       <Title>관리자 비밀번호 입력</Title>
       <LabeledInput
         label=""
         placeholder="현재 비밀번호를 입력하세요."
-        value=""
+        value={currentPW}
         onChange={(e) => setCurrentPW(e.target.value)}
       />
+      <Button>확인</Button>
+      <ChangePW>비밀번호 변경</ChangePW>
     </PasswordForm>
   );
 };
@@ -24,4 +29,12 @@ const Title = styled.h2`
   margin-bottom: 30px;
   font-size: 32px;
   font-weight: 700;
+`;
+
+const ChangePW = styled.button`
+  border: none;
+  color: #434f5a;
+  font-size: 16px;
+  background: none;
+  margin-top: 12px;
 `;
