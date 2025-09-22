4681d30eccbed6972079cdf2947e9d0eb7bec5a6
hyemin <ohyemin1113@gmail.com>
2025-09-22 23:27:14 +0900

    feat : 이용 기록 추가 페이지 구현

---
+++ src/pages/AddUsageForm.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import VisitForm from '../components/VisitForm';
+
+const AddUsageForm = () => {
+  return (
+    <>
+      <Header>이용 기록 추가</Header>
+      <VisitForm />
+    </>
+  );
+};
+
+export default AddUsageForm;
