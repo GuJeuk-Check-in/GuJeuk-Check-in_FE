d2181d9a592c8107a9ec2add0283561f9076776b
hyemin <ohyemin1113@gmail.com>
2025-09-22 23:32:30 +0900

    feat : UseCard 컴포넌트 구조 및 기능 개선

--- src/components/UseCard.jsx
+++ src/components/UseCard.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import styled from '@emotion/styled';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
-const UseCard = ({ date, usename, visitorount }) => {
+const UseCard = ({ date, useName, maleCount, femaleCount }) => {
+  const [alertOpen, setAlertOpen] = useState(false);
   return (
     <Container>
-      <Date>{date}</Date>
-      <UseName>{usename}</UseName>
-      <VisitorCount>{visitorount}</VisitorCount>
-      <DeleteIcon>삭제 아이콘</DeleteIcon>
+      <Section>
+        <Date>{date}</Date>
+        <BottomLine>
+          <UseName>{useName}</UseName>
+          <VisitorCount>
+            남: {maleCount} | 여: {femaleCount}
+          </VisitorCount>
+        </BottomLine>
+      </Section>
+      <DeleteIcon onClick={() => setAlertOpen(true)}>
+        <RiDeleteBin5Line />
+      </DeleteIcon>
     </Container>
   );
 };
@@ -15,28 +27,54 @@ export default UseCard;
 
 const Container = styled.div`
   background-color: #ffffff;
-  width: 1200px;
-  height: 161px;
+  width: 89%;
+  height: auto;
   border-radius: 36px;
   border: 1px solid #2e2e32;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   margin-left: 44px;
-  gap: 20px;
   margin-bottom: 20px;
+  padding: 20px 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const BottomLine = styled.div`
+  display: flex;
+  align-items: flex-start;
 `;
 
 const Date = styled.p`
-  color: red;
+  color: #969698;
+  font-size: 20px;
 `;
 
 const UseName = styled.h2`
-  color: black;
+  color: #2e2e32;
+  font-size: 32px;
+  margin: 0;
+  margin-right: 200px;
+  margin-top: 0;
 `;
 
 const VisitorCount = styled.p`
-  color: gray;
+  color: #2e2e32;
+  font-size: 20px;
+  margin: 0;
+  margin-bottom: 30px;
 `;
 
-const DeleteIcon = styled.div`
-  color: brown;
+const DeleteIcon = styled.button`
+  display: flex;
+  color: #d88282;
+  font-size: 28px;
+  border: none;
+  background: none;
+  cursor: pointer;
 `;
