eb26c3c9fef0aa48e44cbcfbd6603267e97a6310
hyemin <ohyemin1113@gmail.com>
2025-09-24 23:55:19 +0900

    feat : UserDetail 페이지 구현

---
+++ src/pages/UserDetail.jsx
@@ -0,0 +1,13 @@
+import VisitForm from '../components/VisitForm';
+import Header from '../components/Header';
+
+const UserDetail = () => {
+  return (
+    <>
+      <Header>이용 기록 상세 및 수정</Header>
+      <VisitForm />
+    </>
+  );
+};
+
+export default UserDetail;
