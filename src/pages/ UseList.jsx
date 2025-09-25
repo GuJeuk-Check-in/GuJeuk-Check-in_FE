c8a5323d4a608bdacc21887863db609513f49a4a
hyemin <ohyemin1113@gmail.com>
2025-09-25 17:48:19 +0900

    feat : UseList 페이지에서 카드 클릭 시 UserDetail 페이지로 이동 기능 추가

--- src/pages/ UseList.jsx
+++ src/pages/ UseList.jsx
@@ -1,7 +1,13 @@
 import Header from '../components/Header';
 import UseCard from '../components/UseCard';
+import { useNavigate } from 'react-router-dom';
 
 const UseList = () => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate('/user-detail');
+  };
   return (
     <>
       <Header>이용 목록</Header>
@@ -10,12 +16,14 @@ const UseList = () => {
         useName="오혜민"
         maleCount="3"
         femaleCount="1"
+        onClick={handleCardClick}
       ></UseCard>
       <UseCard
         date="2025년 11월 13일"
         useName="김정욱"
         maleCount="3"
         femaleCount="1"
+        onClick={handleCardClick}
       ></UseCard>
       <UseCard
         date="2025년 11월 13일"
